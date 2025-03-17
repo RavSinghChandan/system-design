356f19fa00a080440b4533a1070626cca757e510
Rav Chandan Singh <ravchandan15@gmail.com>
2025-03-17 08:30:36 +0530

    done

--- structural/src/proxy/Client.java
+++ structural/src/proxy/Client.java
@@ -1,4 +1,32 @@
 package proxy;
 
 public class Client {
+    /*
+
+    Adapter → Conversion
+    Bridge → Decoupling
+    Composite → Hierarchy
+    Decorator → Extension
+    Facade → Simplification
+    Flyweight → Optimization
+    Proxy → Control
+
+    Singleton → Uniqueness
+    Factory Method → Instantiation
+    Abstract Factory → Families
+    Builder → Construction
+    Prototype → Cloning
+
+    Chain of Responsibility → Delegation
+    Command → Encapsulation
+    Interpreter → Parsing
+    Iterator → Traversal
+    Mediator → Coordination
+    Memento → Snapshot
+    Observer → Subscription
+    State → Transition
+    Strategy → Selection
+    Template Method → Blueprint
+    Visitor → Extension
+    */
 }
